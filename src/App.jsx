f071a3f38f413ed3ad7b742e1f06b66cd878ce0c
Tomislav Dukez <tomdu3@ymail.com>
2025-02-23 19:51:24 +0000

    update App with Cart context

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import ProductList from "./components/ProductList";
 import Cart from "./components/Cart";
+import { CartProvider } from "./context/CartContext";
 
 function App() {
   return (
-    <Router>
+    <CartProvider>
+      <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<ProductList />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
+    </CartProvider>
   );
 }
 
