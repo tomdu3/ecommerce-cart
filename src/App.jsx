d62f4e0e59f5aebaf514fb800cc458d4ba88b693
Tomislav Dukez <tomdu3@ymail.com>
2025-02-23 11:32:02 +0000

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-    </>
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+      </Routes>
+    </Router>
   );
 }
 
